dd8b1edd66359f327a4847f785b5269d9d7943cf
Praveen Kumar <xav.axp@gmail.com>
2019-03-16 11:39:31 +0530

    init commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('client'));
+
+app.get('/', function (req, res) {
+
+    res.sendfile('./client/index.html');
+
+});
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
