15a2b7e6c030f4033ca1bb1a53fe754c8f39032d
Praveen Kumar <xav.axp@gmail.com>
2019-03-16 14:28:38 +0530

    Added latest changes to index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const nlp = require('./nlp');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(express.static('client'));
 
@@ -12,5 +18,24 @@ app.get('/', function (req, res) {
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/nlp', (req, res) => {
+    
+    
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+    
+    console.log({
+        user_id:user_id,
+        token:token,
+        geo:geo
+    });
+    res.send('Hello World!')
+
+
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
